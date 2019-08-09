ff23239458ccb74cf2267e7efb9ec220e717736c
fengyu <sirfengyu@gmail.com>
2019-08-09 14:19:52 +0800

    main.js三种写法

--- src/main.js
+++ src/main.js
@@ -5,8 +5,28 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+//  第一种写法
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// 第二种写法
+// new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   template: '<App></App>',
+//   components: {
+//       App
+//   }
+// })
+
+// 第三种写法
+// new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   render: h => h(App)
+// })
